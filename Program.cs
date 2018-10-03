e145de7ba02e5f52ce5025985bdc900516c6affc
kazezx <32035020+kazezx@users.noreply.github.com>
2018-10-03 19:26:38 +0400

    Add files via upload
    Time Manipulator

--- Program.cs
+++ Program.cs
@@ -1,65 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.OleDb;
-using System.Linq;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> Licenses = new List<string>();
-            List<int> counter = new List<int>();
-            Console.Write("Enter Your Domain : ");
-            string domain = Console.ReadLine().ToUpper();
-            string query = "SELECT [License],[Counter] FROM ["+domain+"] WHERE ";
-            Console.Write("Enter Your Query : ");
-            string[] services = Console.ReadLine().Split(';');
-            int query_size = services.Length - 1;
-            for (int i = 0; i < services.Length; i++)
-            {
-                services[i] = (services[i].Replace(" ", String.Empty))+"=" + "'" + "Yes" + "'";
-                if (i < query_size)     
-                {
-                    services[i] = services[i] + " AND ";
-                }
-                query = query + services[i];
+            string x_date = "";
 
-            }
-            query = query + "ORDER BY Counter ASC" + ";";
-            Console.WriteLine(query);
-            Console.ReadLine(); 
-            string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Kaze\Documents\GRDF.mdb";
-            string strSQL = query; 
-            using (OleDbConnection connection = new OleDbConnection(connectionString))
-            { 
-                OleDbCommand command = new OleDbCommand(strSQL, connection);
-                try
-                { 
-                    connection.Open();
-                    using (OleDbDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            Licenses.Add(reader.GetValue(0).ToString());
-                            counter.Add(Convert.ToInt32(reader.GetValue(1)));
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-            try
+            DateTime today = DateTime.Today;
+            DateTime yesterday = DateTime.Now.AddDays(-1);
+
+            string today_date = today.ToString("dd/MM/yyyy");
+            string yesterday_date = yesterday.ToString("dd/MM/yyyy");
+
+            TimeSpan start = new TimeSpan(9, 0, 0);
+            TimeSpan end = new TimeSpan(24, 0, 0);
+            TimeSpan now = DateTime.Now.TimeOfDay;
+
+            if ((now > start) && (now < end))
             {
-                int index = counter.Min();
-                string lic = Licenses[counter.IndexOf(index)];
-                Console.WriteLine(lic);
+                x_date = today_date.Replace('/','.');
             }
-            catch (Exception) { Console.WriteLine("No such Licenses available"); }
-            Console.ReadKey();
+            else
+            {  
+                x_date = yesterday_date.Replace('/', '.');
+            }
+            Console.WriteLine(x_date);
+            Console.Read();
 
         }
     }
